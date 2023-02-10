b7684dcbdd0505a1b453cee5be0ddcd72dcff2fa
Andrei Patriota <63430047+AndreiPatriota@users.noreply.github.com>
2023-02-10 12:17:25 -0300

    Fix paginator bug
    Pagination was not working properly when changing the number of rows to be shown.

--- src/app/views/components/tecnicos/tecnico-read/tecnico-read.component.ts
+++ src/app/views/components/tecnicos/tecnico-read/tecnico-read.component.ts
@@ -1,6 +1,6 @@
 import { AfterViewInit, Component, ViewChild } from '@angular/core';
 import { MatPaginator } from '@angular/material/paginator';
-import { MatTableDataSource } from '@angular/material/table';
+import { MatTableDataSource, MatTableDataSourcePaginator } from '@angular/material/table';
 import { Tecnico } from 'src/app/models/tecnico';
 import { TecnicoService } from 'src/app/services/tecnico.service';
 
@@ -11,24 +11,24 @@ import { TecnicoService } from 'src/app/services/tecnico.service';
 })
 export class TecnicoReadComponent implements AfterViewInit {
 
-  listadeTecnicos: Tecnico[] = [];
+  listadeTecnicos: Tecnico[] = []
 
-  displayedColumns: string[] = ['id', 'nome', 'cpf', 'telefone'];
-  dataSource = new MatTableDataSource<Tecnico>(this.listadeTecnicos);
+  displayedColumns: string[] = ['id', 'nome', 'cpf', 'telefone']
+  dataSource = new MatTableDataSource<Tecnico>(this.listadeTecnicos)
 
-  @ViewChild(MatPaginator) paginator!: MatPaginator;
+  @ViewChild(MatPaginator) paginator!: MatPaginator
 
-  constructor(private service: TecnicoService) {}
+  constructor(private service: TecnicoService) { }
 
   ngAfterViewInit() {
-    this.dataSource.paginator = this.paginator;
-    this.findAll();
+    this.findAll()
   }
 
   findAll() {
     this.service.findAll().subscribe((resposta) => {
-      this.listadeTecnicos = resposta;
-      console.log(this.listadeTecnicos);
+      this.listadeTecnicos = resposta
+      this.dataSource = new MatTableDataSource<Tecnico>(this.listadeTecnicos)
+      this.dataSource.paginator = this.paginator
     });
   }
 }
